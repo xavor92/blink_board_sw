f9ec5a548d44e15fb5a8e85e90b4dacb7dc731c5
Oliver Westermann <owestermann@googlemail.com>
2024-02-04 20:11:20 +0100

    working fetch
    based on https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/HTTPSRequest/HTTPSRequest.ino

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,100 @@
-#include <Arduino.h>
+/*
+    HTTP over TLS (HTTPS) example sketch
+
+    This example demonstrates how to use
+    WiFiClientSecure class to access HTTPS API.
+    We fetch and display the status of
+    esp8266/Arduino project continuous integration
+    build.
+
+    Created by Ivan Grokhotkov, 2015.
+    This example is in public domain.
+*/
 
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include "certs.h"
 
-#include "gpio_handling.hpp"
-#include "mqtt.hpp"
-#include "wifi.hpp"
+#ifndef STASSID
+#define STASSID "x"
+#define STAPSK "x"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+X509List cert(cert_ISRG_Root_X1);
 
 void setup() {
   Serial.begin(115200);
-  setup_gpio();
-  setup_wifi();
-  setup_mqtt();
-}
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-void loop()
-{
-  static unsigned long next_blink;
-  const unsigned long blink_period = 10000;
-  const unsigned long blink_on_phase = 100;
+  // Set time via NTP, as required for x.509 validation
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
 
-  // blink LED every 5s for 500ms
-  unsigned long now = millis();
-  if (now > next_blink) {
-    next_blink = now + blink_period;
-    gpio_set_led(PIN_BLINK_LED, ON);
-    queued_led_change_t blink_led_off = {
-      .led_pin = PIN_BLINK_LED,
-      .led_state = OFF,
-      .timestamp = now + blink_on_phase
-    };
-    queue_led_change(blink_led_off);
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("Connecting to ");
+  Serial.println(owesterm_host);
+
+  Serial.printf("Using certificate: %s\n", cert_ISRG_Root_X1);
+  client.setTrustAnchors(&cert);
+
+  if (!client.connect(owesterm_host, owesterm_port)) {
+    Serial.println("Connection failed");
+    return;
   }
 
-  mqtt_loop();
+  String url = "/api/firmware/blinkboard/firmware.elf";
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
 
-  check_button_and_publish();
-  handle_led_queue();
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + owesterm_host + "\r\n" + "User-Agent: BuildFailureDetectorESP8266\r\n" + "Connection: close\r\n\r\n");
+
+  Serial.println("Request sent");
+  while (client.available()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("Headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+    Serial.println("esp8266/Arduino CI successful!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("Reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  Serial.println("==========");
+  Serial.println("Closing connection");
 }
+
+void loop() {}
