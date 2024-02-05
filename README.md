da0a535707131561dbfeb850a551db0082aba4bb
Oliver Westermann <owestermann@googlemail.com>
2024-02-05 22:31:59 +0100

    Updated README.md, Bugfix name storage

--- README.md
+++ README.md
@@ -6,22 +6,39 @@ Software for the [BLINK Board](https://github.com/xavor92/blink_board_hw)
 
 Use VSCode with [PlatformIO](https://platformio.org/) installed.
 
-Should work OOTB.
-
-## secrets.h
+### secrets.hpp
 
 See the template. Setup your own MQTT server or check with your team for credentials ;-)
 
-## Programming mode
+# Board usage
 
-Keep Boot pressed while power cycling/resetting the board with reset pin.
-Refer to hardware repo for button positions.
+## Debugging
+
+UART should be labeled on V1.2 of the board, otherwise it's the three-pin header.
+
+Top to Bottom
+* TXD
+* RXD
+* GND
+
+## Flashing
+
+* Set board into programming mode
+  keep *Boot* pressed while power cycling/resetting the board with *Reset*.
+* Use flash method from platformIO or esptool.py
+
+# Setup
+
+When the board can not connect to a WiFi on boot, it will start a WiFi with no password and a captive portal at 192.168.4.1 to allow setting up WiFi and the name of the Board owner. The same captive portal can be started by keeping SW1 and SW2 pressed at boot.
 
 ## Functions
+
+* Press SW1 to turn on RED LEDs on all boards
+* Press SW2 to turn on GREEN LEDs on all boards
 
 ### Bootup LEDs
 
 1. LED Test (All LEDs should flash once)
 2. RED1 turns on -> WiFi connection attempt
 3. RED1 turns off, GREEN1 turns on -> Wifi connected
-3. RED2 will be on if Wifi is not conencted
+3. RED2 will be on if MQTT fails to connect/is reconnecting
--- src/wifi.cpp
+++ src/wifi.cpp
@@ -7,7 +7,8 @@
 
 WiFiClient wifi_client;
 WiFiManagerParameter name("name", "Your Name", "", 16);
-char eeprom_name[17];
+const char name_len = 17;
+char eeprom_name[name_len];
 
 void setup_wifi() {
   bool res;
@@ -16,17 +17,36 @@ void setup_wifi() {
   WiFi.mode(WIFI_STA);
   WiFiManager wm;
 
-  // Load potentially saved name from EEPROM
+  // Load potentially saved name from EEPROM, save one space for null terminator
   EEPROM.begin(512);
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < name_len - 1; i++) {
     eeprom_name[i] = EEPROM.read(i);
   }
-  for (int i = 0; i < 16; i++) {
+  eeprom_name[name_len - 1] = 0; // null terminator
+
+  // check if eeprom_name contains a valid string
+  res = false;
+  for (int i = 0; i < name_len; i++) {
+    if (eeprom_name[i] == 0) {
+      // end of string -> valid
+      Serial.println("Found end of string at " + String(i));
+      res = true;
+      break;
+    }
     if (eeprom_name[i] < 32 || eeprom_name[i] > 126) {
+      // not a valid char -> invalid
+      Serial.println("Not valid at " + String(i) + ": " + String(eeprom_name[i]));
+      break;
+    }
+  }
+
+  if (!res) {
+    // if not, set it to an empty string
+    Serial.println("Wiping String");
+    for (int i = 0; i < name_len; i++) {
       eeprom_name[i] = 0;
     }
   }
-  eeprom_name[16] = '\0';
   name.setValue(eeprom_name, 16);
   Serial.println("Loaded custom parameter: " + String(eeprom_name));
 
@@ -56,10 +76,11 @@ void setup_wifi() {
 
     // Get the custom text value
     const char* custom_value = name.getValue();
+    Serial.println("Updated custom parameter: " + String(eeprom_name));
 
     // Only write to EEPROM if the value has changed
     if (strcmp(custom_value, eeprom_name) != 0) {
-      for (int i = 0; i < 16; i++) {
+      for (int i = 0; i < name_len - 1; i++) {
         EEPROM.write(i, custom_value[i]);
       }
       EEPROM.commit(); // Make sure to commit changes
